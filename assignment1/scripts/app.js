c3d1b4841f6f189a5c0cabcc69c47b03f2597175
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2020-05-04 02:06:03 +1200

    Implement the requierments

--- assignment1/scripts/app.js
+++ assignment1/scripts/app.js
@@ -6,5 +6,33 @@
     .controller('LunchCheckController', LunchCheckController);
 
   LunchCheckController.$inject = ['$scope'];
-  function LunchCheckController($scope) {}
+  function LunchCheckController($scope) {
+    $scope.lunch = '';
+    $scope.message = '';
+    $scope.message_class = '';
+    $scope.border_class = '';
+    $scope.check = function () {
+      var items = $scope.lunch
+        .split(',')
+        .map((item) => item.trim())
+        .filter((item) => !!item);
+      var items_count = items.length;
+      console.log('items', items);
+      if (items_count < 1) {
+        $scope.message = 'Please enter data first';
+        $scope.message_class = 'text-danger';
+        $scope.border_class = 'border border-danger';
+        return;
+      }
+      if (items_count > 3) {
+        $scope.message = 'Too much!';
+        $scope.message_class = 'text-success';
+        $scope.border_class = 'border border-success';
+        return;
+      }
+      $scope.message = 'Enjoy!';
+      $scope.message_class = 'text-success';
+      $scope.border_class = 'border border-success';
+    };
+  }
 })();
