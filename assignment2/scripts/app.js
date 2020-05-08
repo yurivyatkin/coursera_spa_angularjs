189b4b98ee2a78ccac0bcfe2f0ee77c567067346
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2020-05-09 00:01:41 +1200

    Make 2 controllers, service, dynamic data; formatting

--- assignment2/scripts/app.js
+++ assignment2/scripts/app.js
@@ -1,5 +1,38 @@
 (function () {
   'use strict';
 
-  angular.module('ShoppingListCheckOff', []);
+  angular
+    .module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var toBuy = this;
+    toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+  }
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var bought = this;
+    bought.items = ShoppingListCheckOffService.getBoughtItems();
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+    var initialItems = [
+      { name: 'cookies', quantity: 10 },
+      { name: 'sausages', quantity: 10 },
+      { name: 'eggs', quantity: 10 },
+    ];
+    var toBuyItems = initialItems;
+    var boughtItems = [];
+    service.getToBuyItems = function () {
+      return toBuyItems;
+    };
+    service.getBoughtItems = function () {
+      return boughtItems;
+    };
+  }
 })();
