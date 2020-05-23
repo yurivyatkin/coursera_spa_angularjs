60d69b600e053ba98fde0c9a8080587aa96de248
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2020-05-23 16:16:47 +1200

    Add routes

---
+++ assignment4/src/routes.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  angular.module('MenuApp').config(RoutesConfig);
+
+  RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function RoutesConfig($stateProvider, $urlRouterProvider) {
+    // Redirect to home page if no other URL matches
+    $urlRouterProvider.otherwise('/');
+  }
+})();
